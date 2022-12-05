6774916ff814a4c24f9487d1e35d0da4b052a233
Paola De Bartolo <paola.debartolo@gmail.com>
2022-12-05 12:02:29 -0300

    build: update web component version to 1.5.1

--- enhanced-rich-text-editor/src/main/java/com/vaadin/componentfactory/GeneratedEnhancedRichTextEditor.java
+++ enhanced-rich-text-editor/src/main/java/com/vaadin/componentfactory/GeneratedEnhancedRichTextEditor.java
@@ -220,7 +220,7 @@ import elemental.json.impl.JreJsonObject;
         "WebComponent: Vaadin.RichTextEditorElement#1.0.0-alpha3",
         "Flow#1.2-SNAPSHOT" })
 @Tag("vcf-enhanced-rich-text-editor")
-@NpmPackage(value = "@vaadin-component-factory/vcf-enhanced-rich-text-editor", version = "1.5.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-enhanced-rich-text-editor", version = "1.5.1")
 @JsModule("@vaadin-component-factory/vcf-enhanced-rich-text-editor/src/vcf-enhanced-rich-text-editor.js")
 public abstract class GeneratedEnhancedRichTextEditor<R extends GeneratedEnhancedRichTextEditor<R, T>, T>
         extends AbstractSinglePropertyField<R, T> implements HasStyle, HasTheme,
