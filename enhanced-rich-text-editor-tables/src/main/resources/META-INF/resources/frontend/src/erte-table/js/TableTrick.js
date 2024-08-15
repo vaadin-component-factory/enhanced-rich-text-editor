632f38ad743e3b2cae173f0bee5814b9e4db48bb
Stefan Uebe <stefan@vaadin.com>
2024-08-15 03:54:55 +0200

    Fix for non updating table toolbar buttons on table insert / remove

--- enhanced-rich-text-editor-tables/src/main/resources/META-INF/resources/frontend/src/erte-table/js/TableTrick.js
+++ enhanced-rich-text-editor-tables/src/main/resources/META-INF/resources/frontend/src/erte-table/js/TableTrick.js
@@ -745,10 +745,10 @@ export default class TableTrick {
           return true;
       }
 
-      TableSelection.cellSelectionChange(quill);
+    }
+      TableSelection.selectionChange(quill);
       // TODO fire a proper event, that informs about new indices, but does
     }
-  }
 
   static getRichTextEditorInstance(quill) {
     // just assure, that our quill is already attached
